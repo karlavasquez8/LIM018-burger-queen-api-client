03f86a00b3e1c06640ccba1b8cbd3685a0fdc377
karla vasquez <karlavasquez817@gmail.com>
2022-10-01 20:36:58 -0500

    feat:configuracion del package

---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,25 @@
+import logo from 'img/logo.png'
+import './login.css'
+
+function Login() {
+    return (
+        <div className="Login">
+            <header className="Login-header">
+                <img src={logo} className="Login-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
+}
+
+export default Login;
