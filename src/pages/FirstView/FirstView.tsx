57fdf1539e6aef81362069d2e353d25d934ef5cf
karla vasquez <karlavasquez817@gmail.com>
2022-10-06 09:58:37 -0500

    feat:avance de la segunda vista

--- src/pages/FirstView/FirstView.tsx
+++ src/pages/FirstView/FirstView.tsx
@@ -13,8 +13,10 @@ function FirstView() {
                     </div>
 
                     <div className='Option-nav'>
-                        <button className="Login-btn"> Menú </button>
-                        <button className="Login-btn"> Ordenes </button>
+                        <div>
+                            <button className="Login-btn"> Menú </button>
+                            <button className="Login-btn"> Ordenes </button>
+                        </div>
                         <img src={logoCocinero} className="Logo-cocinero" alt="logo" />
                     </div>
                 </div>
@@ -24,24 +26,32 @@ function FirstView() {
                     <div className="wrap-container">
                         <div className="container-menu">
                             <div className='items'>
-                                <div className='encabezado-items'>
+                                <div className='square-btn'>
                                     <button className="Login-btn"> Desayuno </button>
                                     <button className="Login-btn"> Almuerzo y Cena </button>
                                 </div>
                                 <section className='info-items'>
                                     <div className="img-item">
-                                        <img src={hamburguesaprueba} className="item" alt="" />
+                                        <img src={hamburguesaprueba} className="img-item" alt="" />
+                                        <span>Sandwich de jamon y queso </span>
                                     </div>
                                     <div className='data-items'>
-                                        <span>Sandwich de jamon y queso </span>
-                                        <span>$10</span>
+                                        <span className='cash-item'>$10</span>
                                     </div>
-
-
                                 </section>
                             </div>
-                            <div className='pedido'>
-
+                            <div className='items'>
+                                <section>
+                                    <div>
+                                        <span> CLIENTE:</span>
+                                        <span> nombre del cliente</span>
+                                    </div>
+                                    <div>
+                                        <span> ITEM</span>
+                                        <span> CANT</span>
+                                        <span> PRECIO</span>
+                                    </div>
+                                </section>
                             </div>
                         </div>
                     </div>
