da723ac1548a8dbdba8208ea3f9ce000edd61b26
karla vasquez <karlavasquez817@gmail.com>
2022-10-13 12:32:26 -0500

    feat: se ordeno los cards y se hizo el db jason

---
+++ src/pages/FirstView/CardItems.tsx
@@ -0,0 +1,21 @@
+interface CardItemsProps {
+    name: string;
+    img: string;
+    price: number;
+}
+
+function CardItems({ name, price, img }: CardItemsProps) {
+    return (
+        <section className='info-items'>
+            <div className="img-item">
+                <img src={img} className="img-item" alt="" />
+                <span>{name}</span>
+            </div>
+            <div className='data-items'>
+                <span className='cash-item'>{price}</span>
+            </div>
+        </section>
+    )
+}
+
+export default CardItems
--- src/pages/FirstView/FirstView.tsx
+++ src/pages/FirstView/FirstView.tsx
@@ -1,19 +1,7 @@
 import logo from 'img/logo.png'
 import logoCocinero from 'img/logo-cocinero.png'
-import hamburguesaprueba from 'img/hamburguesaprueba.png'
-import cafe from 'img/cafe.png'
-import cafeconleche from 'img/cafeconleche.png'
-import sandwich_panino from 'img/SANDWICH_PANINO.png'
-import jugo from 'img/jugo.png'
-import hambursimple from 'img/hambur-simple.png'
-import hamburdoble from 'img/hambur-doble.png'
-import papas from 'img/papitas.png'
-import cebolla from 'img/cebollas.png'
-import gaseosa500 from 'img/gaseosa-500.png'
-import gaseosa750 from 'img/gaseosa-750.png'
-import agua500 from 'img/agua-500.png'
-import agua750 from 'img/agua-750.png'
 import './firstView.css'
+import CardItems from './CardItems'
 
 function FirstView() {
     return (
@@ -47,120 +35,10 @@ function FirstView() {
                                     <button className="Login-btn"> Acompañamientos </button>
                                     <button className="Login-btn"> Para tomar </button>
                                 </div>
-                                    <div className='fondo-items'>
-                                        <section className='info-items'>
-                                            <div className="img-item">
-                                                <img src={cafe} className="img-item" alt="" />
-                                                <span>Cáfe </span>
-                                            </div>
-                                            <div className='data-items'>
-                                                <span className='cash-item'>$5</span>
-                                            </div>
-                                        </section>
-                                        <section className='info-items'>
-                                            <div className="img-item">
-                                                <img src={cafeconleche} className="img-item" alt="" />
-                                                <span>Cafe con Leche </span>
-                                            </div>
-                                            <div className='data-items'>
-                                                <span className='cash-item'>$7</span>
-                                            </div>
-                                        </section>
-                                        <section className='info-items'>
-                                            <div className="img-item">
-                                                <img src={sandwich_panino} className="img-item" alt="" />
-                                                <span>Sandwich de jamon y queso </span>
-                                            </div>
-                                            <div className='data-items'>
-                                                <span className='cash-item'>$10</span>
-                                            </div>
-                                        </section>
-                                        <section className='info-items'>
-                                            <div className="img-item">
-                                                <img src={jugo} className="img-item" alt="" />
-                                                <span>Jugo Natural </span>
-                                            </div>
-                                            <div className='data-items'>
-                                                <span className='cash-item'>$7</span>
-                                            </div>
-                                        </section>
-                                    </div>
-                                    <div className='fondo-items'>
-                                        <section className='info-items'>
-                                            <div className="img-item">
-                                                <img src={papas} className="img-item" alt="" />
-                                                <span>Papas Fritas </span>
-                                            </div>
-                                            <div className='data-items'>
-                                                <span className='cash-item'>$5</span>
-                                            </div>
-                                        </section>
-                                        <section className='info-items'>
-                                            <div className="img-item">
-                                                <img src={cebolla} className="img-item" alt="" />
-                                                <span>Aros de Cebolla</span>
-                                            </div>
-                                            <div className='data-items'>
-                                                <span className='cash-item'>$5</span>
-                                            </div>
-                                        </section>
-                                        <section className='info-items'>
-                                            <div className="img-item">
-                                                <img src={hambursimple} className="img-item" alt="" />
-                                                <span>Hamburguesa Simple </span>
-                                            </div>
-                                            <div className='data-items'>
-                                                <span className='cash-item'>$10</span>
-                                            </div>
-                                        </section>
-                                        <section className='info-items'>
-                                            <div className="img-item">
-                                                <img src={hamburdoble} className="img-item" alt="" />
-                                                <span>Hamburguesa Doble</span>
-                                            </div>
-                                            <div className='data-items'>
-                                                <span className='cash-item'>$15</span>
-                                            </div>
-                                        </section>
-                                    </div>
-                                    <div className='fondo-items'>
-                                        <section className='info-items'>
-                                            <div className="img-item">
-                                                <img src={gaseosa500} className="img-item" alt="" />
-                                                <span> Bebida/Gaseosa 500 ml</span>
-                                            </div>
-                                            <div className='data-items'>
-                                                <span className='cash-item'>$7</span>
-                                            </div>
-                                        </section>
-                                        <section className='info-items'>
-                                            <div className="img-item">
-                                                <img src={gaseosa750} className="img-item" alt="" />
-                                                <span>Bebida/Gaseosa 750 ml</span>
-                                            </div>
-                                            <div className='data-items'>
-                                                <span className='cash-item'>$10</span>
-                                            </div>
-                                        </section>
-                                        <section className='info-items'>
-                                            <div className="img-item">
-                                                <img src={agua500} className="img-item" alt="" />
-                                                <span>Agua de 500ml</span>
-                                            </div>
-                                            <div className='data-items'>
-                                                <span className='cash-item'>$5</span>
-                                            </div>
-                                        </section>
-                                        <section className='info-items'>
-                                            <div className="img-item">
-                                                <img src={agua750} className="img-item" alt="" />
-                                                <span>Agua de 750ml</span>
-                                            </div>
-                                            <div className='data-items'>
-                                                <span className='cash-item'>$7</span>
-                                            </div>
-                                        </section>
-                                    </div>
+                                <div className='fondo-items'>
+                                    <CardItems img='' name='' price={7} />
+
+                                </div>
                             </div>
                             <div className='items'>
                                 <section>
