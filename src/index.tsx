25b5499dca508522c61b11544d01db2ad2fe6fcb
diana gomez <diaartgo@gmail.com>
2022-10-12 22:20:11 -0500

    actualizando maquetado

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import FirstView from './pages/FirstView/FirstView';
+import Login from 'pages/Login/Login';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <FirstView />
+    <Login />
   </React.StrictMode>
 );
 
--- src/pages/FirstView/FirstView.tsx
+++ src/pages/FirstView/FirstView.tsx
@@ -1,6 +1,18 @@
 import logo from 'img/logo.png'
 import logoCocinero from 'img/logo-cocinero.png'
 import hamburguesaprueba from 'img/hamburguesaprueba.png'
+import cafe from 'img/cafe.png'
+import cafeconleche from 'img/cafeconleche.png'
+import sandwich_panino from 'img/SANDWICH_PANINO.png'
+import jugo from 'img/jugo.png'
+import hambursimple from 'img/hambur-simple.png'
+import hamburdoble from 'img/hambur-doble.png'
+import papas from 'img/papitas.png'
+import cebolla from 'img/cebollas.png'
+import gaseosa500 from 'img/gaseosa-500.png'
+import gaseosa750 from 'img/gaseosa-750.png'
+import agua500 from 'img/agua-500.png'
+import agua750 from 'img/agua-750.png'
 import './firstView.css'
 
 function FirstView() {
@@ -30,15 +42,120 @@ function FirstView() {
                                     <button className="Login-btn"> Desayuno </button>
                                     <button className="Login-btn"> Almuerzo y Cena </button>
                                 </div>
-                                <section className='info-items'>
-                                    <div className="img-item">
-                                        <img src={hamburguesaprueba} className="img-item" alt="" />
-                                        <span>Sandwich de jamon y queso </span>
+                                    <div className='fondo-items'>
+                                        <section className='info-items'>
+                                            <div className="img-item">
+                                                <img src={cafe} className="img-item" alt="" />
+                                                <span>Cáfe </span>
+                                            </div>
+                                            <div className='data-items'>
+                                                <span className='cash-item'>$5</span>
+                                            </div>
+                                        </section>
+                                        <section className='info-items'>
+                                            <div className="img-item">
+                                                <img src={cafeconleche} className="img-item" alt="" />
+                                                <span>Cafe con Leche </span>
+                                            </div>
+                                            <div className='data-items'>
+                                                <span className='cash-item'>$7</span>
+                                            </div>
+                                        </section>
+                                        <section className='info-items'>
+                                            <div className="img-item">
+                                                <img src={sandwich_panino} className="img-item" alt="" />
+                                                <span>Sandwich de jamon y queso </span>
+                                            </div>
+                                            <div className='data-items'>
+                                                <span className='cash-item'>$10</span>
+                                            </div>
+                                        </section>
+                                        <section className='info-items'>
+                                            <div className="img-item">
+                                                <img src={jugo} className="img-item" alt="" />
+                                                <span>Jugo Natural </span>
+                                            </div>
+                                            <div className='data-items'>
+                                                <span className='cash-item'>$7</span>
+                                            </div>
+                                        </section>
                                     </div>
-                                    <div className='data-items'>
-                                        <span className='cash-item'>$10</span>
+                                    <div className='fondo-items'>
+                                        <section className='info-items'>
+                                            <div className="img-item">
+                                                <img src={papas} className="img-item" alt="" />
+                                                <span>Papas Fritas </span>
+                                            </div>
+                                            <div className='data-items'>
+                                                <span className='cash-item'>$5</span>
+                                            </div>
+                                        </section>
+                                        <section className='info-items'>
+                                            <div className="img-item">
+                                                <img src={cebolla} className="img-item" alt="" />
+                                                <span>Aros de Cebolla</span>
+                                            </div>
+                                            <div className='data-items'>
+                                                <span className='cash-item'>$5</span>
+                                            </div>
+                                        </section>
+                                        <section className='info-items'>
+                                            <div className="img-item">
+                                                <img src={hambursimple} className="img-item" alt="" />
+                                                <span>Hamburguesa Simple </span>
+                                            </div>
+                                            <div className='data-items'>
+                                                <span className='cash-item'>$10</span>
+                                            </div>
+                                        </section>
+                                        <section className='info-items'>
+                                            <div className="img-item">
+                                                <img src={hamburdoble} className="img-item" alt="" />
+                                                <span>Hamburguesa Doble</span>
+                                            </div>
+                                            <div className='data-items'>
+                                                <span className='cash-item'>$15</span>
+                                            </div>
+                                        </section>
                                     </div>
-                                </section>
+                                    <div className='fondo-items'>
+                                        <section className='info-items'>
+                                            <div className="img-item">
+                                                <img src={gaseosa500} className="img-item" alt="" />
+                                                <span> Bebida/Gaseosa 500 ml</span>
+                                            </div>
+                                            <div className='data-items'>
+                                                <span className='cash-item'>$7</span>
+                                            </div>
+                                        </section>
+                                        <section className='info-items'>
+                                            <div className="img-item">
+                                                <img src={gaseosa750} className="img-item" alt="" />
+                                                <span>Bebida/Gaseosa 750 ml</span>
+                                            </div>
+                                            <div className='data-items'>
+                                                <span className='cash-item'>$10</span>
+                                            </div>
+                                        </section>
+                                        <section className='info-items'>
+                                            <div className="img-item">
+                                                <img src={agua500} className="img-item" alt="" />
+                                                <span>Agua de 500ml</span>
+                                            </div>
+                                            <div className='data-items'>
+                                                <span className='cash-item'>$5</span>
+                                            </div>
+                                        </section>
+                                        <section className='info-items'>
+                                            <div className="img-item">
+                                                <img src={agua750} className="img-item" alt="" />
+                                                <span>Agua de 750ml</span>
+                                            </div>
+                                            <div className='data-items'>
+                                                <span className='cash-item'>$7</span>
+                                            </div>
+                                        </section>
+                                    </div>
                             </div>
                             <div className='items'>
                                 <section>
